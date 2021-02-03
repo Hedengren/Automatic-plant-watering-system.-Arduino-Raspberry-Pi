b090102f3a22fe636f6ce36b88b4e8974fa2a8ff
Hedengren <svenisachedengren@gmail.com>
2021-02-03 15:07:30 +0100

    Add files via upload

--- bevattning/bevattning.ino
+++ bevattning/bevattning.ino
@@ -6,16 +6,32 @@ const int pumpPin2 = ;
 const int pumpPin3 = ; 
 const int pumpPin4 = ;
 
+const int tempAndHumiditySensor = ;
+
 const int soilSensor1 = A4;
 const int soilSensor2 = A4;
 const int soilSensor3 = A4;
 const int soilSensor4 = A4;
 
+
+#include <DHT.h>;
+#define DHTPIN 7 //Kan ändras...
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+int chk;
+float hum; 
+float temp; 
+
 void setup() {
+
+  dht.begin();
   pinMode(pumpPin1, OUTPUT);
   pinMode(pumpPin2, OUTPUT);
   pinMode(pumpPin3, OUTPUT);
   pinMode(pumpPin4, OUTPUT);
+
+  pinMode(tempAndHumiditySensor, INPUT);
   
   pinMode(soilSensor1, INPUT);
   pinMode(soilSensor2, INPUT);
@@ -34,12 +50,23 @@ void setup() {
 void loop() {
 
   delay(180000);//Vänta med att köra loopen..
+
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
   
+  Serial.print("Room humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temperature: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");  
+
   // hämta värdet
   int moisture1 = analogRead(soilSensor1);
   int moisture2 = analogRead(soilSensor2);
   int moisture3 = analogRead(soilSensor3);
   int moisture4 = analogRead(soilSensor4);
+
+  
   
   Serial.println(moisture1);
   Serial.println(moisture2);
@@ -56,7 +83,7 @@ void loop() {
     // kör pump i fem sekunder
     delay(5000);
 
-    // turn off water
+    // avsluta bevattningen 
     digitalWrite(pumpPin1, HIGH);
     Serial.println("Done watering.");
   } else {
@@ -71,7 +98,7 @@ void loop() {
     // kör pump i fem sekunder
     delay(5000);
 
-    // turn off water
+    // avsluta bevattningen 
     digitalWrite(pumpPin2, HIGH);
     Serial.println("Done watering.");
   } else {
@@ -86,7 +113,7 @@ void loop() {
     // kör pump i fem sekunder
     delay(5000);
 
-    // turn off water
+    // avsluta bevattningen 
     digitalWrite(pumpPi3, HIGH);
     Serial.println("Done watering.");
   } else {
@@ -101,8 +128,8 @@ void loop() {
     // kör pump i fem sekunder
     delay(5000);
 
-    // turn off water
-    digitalWrite(pumpPin, HIGH);
+    // avsluta bevattningen 
+    digitalWrite(pumpPin4, HIGH);
     Serial.println("Done watering.");
   } else {
     Serial.println("Startar bevattning..fuktigheten är: " + String(moisture4));
